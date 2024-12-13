5e2be8708bde5741f13a1077977c0978b75908ad
f1nniboy <me@f1nn.space>
2024-12-13 13:54:48 +0100

    update config

--- config.def.h
+++ config.def.h
@@ -4,16 +4,16 @@
                         ((hex >> 8) & 0xFF) / 255.0f, \
                         (hex & 0xFF) / 255.0f }
 /* appearance */
-static const int sloppyfocus               = 1;  /* focus follows mouse */
-static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
-static const int smartborders              = 1;
-static const unsigned int borderpx         = 1;  /* border pixel of windows */
-static const float rootcolor[]             = COLOR(0x222222ff);
-static const float bordercolor[]           = COLOR(0x444444ff);
-static const float focuscolor[]            = COLOR(0x005577ff);
-static const float urgentcolor[]           = COLOR(0xff0000ff);
+static const int sloppyfocus				= 1;  /* focus follows mouse */
+static const int bypass_surface_visibility	= 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
+static const int smartborders				= 1;
+static const unsigned int borderpx			= 2;  /* border pixel of windows */
+static const float rootcolor[]				= COLOR(0x11111bff);
+static const float bordercolor[]			= COLOR(0x181825ff);
+static const float focuscolor[]				= COLOR(0xf38ba8ff);
+static const float urgentcolor[]			= COLOR(0xfab387ff);
 /* This conforms to the xdg-protocol. Set the alpha to zero to restore the old behavior */
-static const float fullscreen_bg[]         = {0.1f, 0.1f, 0.1f, 1.0f}; /* You can also use glsl colors */
+static const float fullscreen_bg[]			= {0.1f, 0.1f, 0.1f, 1.0f}; /* You can also use glsl colors */
 
 /* tagging - TAGCOUNT must be no greater than 31 */
 #define TAGCOUNT (9)
@@ -44,21 +44,14 @@ static const Layout layouts[] = {
 */
 /* NOTE: ALWAYS add a fallback rule, even if you are completely sure it won't be used */
 static const MonitorRule monrules[] = {
-	/* name       mfact  nmaster scale layout       rotate/reflect                x    y */
-	/* example of a HiDPI laptop monitor:
-	{ "eDP-1",    0.5f,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
-	*/
-	/* defaults */
-	{ NULL,       0.55f, 1,      1,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
+	/* name	mfact	nmaster	scale	layout			rotate/reflect				x	y */
+	{ NULL,	0.6f,	1,		1,		&layouts[0],	WL_OUTPUT_TRANSFORM_NORMAL,	-1,	-1 },
 };
 
 /* keyboard */
 static const struct xkb_rule_names xkb_rules = {
-	/* can specify fields: rules, model, layout, variant, options */
-	/* example:
-	.options = "ctrl:nocaps",
-	*/
 	.options = NULL,
+	.layout = "de",
 };
 
 static const int repeat_rate = 25;
@@ -98,7 +91,7 @@ static const uint32_t send_events_mode = LIBINPUT_CONFIG_SEND_EVENTS_ENABLED;
 LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT
 LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE
 */
-static const enum libinput_config_accel_profile accel_profile = LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE;
+static const enum libinput_config_accel_profile accel_profile = LIBINPUT_CONFIG_ACCEL_PROFILE_FLAT;
 static const double accel_speed = 0.0;
 
 /* You can choose between:
@@ -108,7 +101,7 @@ LIBINPUT_CONFIG_TAP_MAP_LMR -- 1/2/3 finger tap maps to left/middle/right
 static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TAP_MAP_LRM;
 
 /* If you want to use the windows key for MODKEY, use WLR_MODIFIER_LOGO */
-#define MODKEY WLR_MODIFIER_ALT
+#define MODKEY WLR_MODIFIER_LOGO
 
 #define TAGKEYS(KEY,SKEY,TAG) \
 	{ MODKEY,                    KEY,            view,            {.ui = 1 << TAG} }, \
@@ -121,47 +114,57 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "foot", NULL };
-static const char *menucmd[] = { "wmenu-run", NULL };
+static const char *menucmd[] = { "/home/etc/scripts/launcher", NULL };
+static const char *volupcmd[] = { "/home/etc/scripts/vol", "+", NULL };
+static const char *voldowncmd[] = { "/home/etc/scripts/vol", "-", NULL };
+static const char *shotcmd[] = { "/home/etc/scripts/screenshot", NULL };
+static const char *playertogglecmd[] = { "/home/etc/scripts/player", "play-pause", NULL };
+static const char *playerprevcmd[] = { "/home/etc/scripts/player", "previous", NULL };
+static const char *playernextcmd[] = { "/home/etc/scripts/player", "next", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
-	/* modifier                  key                 function        argument */
-	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,     spawn,          {.v = termcmd} },
-	{ MODKEY,                    XKB_KEY_b,          togglebar,      {0} },
-	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
-	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_J,          movestack,      {.i = +1} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_K,          movestack,      {.i = -1} },
-	{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
-	{ MODKEY,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
-	{ MODKEY,                    XKB_KEY_h,          setmfact,       {.f = -0.05f} },
-	{ MODKEY,                    XKB_KEY_l,          setmfact,       {.f = +0.05f} },
-	{ MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
-	{ MODKEY,                    XKB_KEY_Tab,        view,           {0} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,     {0} },
-	{ MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                    XKB_KEY_f,          setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
-	{ MODKEY,                    XKB_KEY_e,         togglefullscreen, {0} },
-	{ MODKEY,                    XKB_KEY_0,          view,           {.ui = ~0} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,            {.ui = ~0} },
-	{ MODKEY,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
-	{ MODKEY,                    XKB_KEY_period,     focusmon,       {.i = WLR_DIRECTION_RIGHT} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_less,       tagmon,         {.i = WLR_DIRECTION_LEFT} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_greater,    tagmon,         {.i = WLR_DIRECTION_RIGHT} },
-	TAGKEYS(          XKB_KEY_1, XKB_KEY_exclam,                     0),
-	TAGKEYS(          XKB_KEY_2, XKB_KEY_at,                         1),
-	TAGKEYS(          XKB_KEY_3, XKB_KEY_numbersign,                 2),
-	TAGKEYS(          XKB_KEY_4, XKB_KEY_dollar,                     3),
-	TAGKEYS(          XKB_KEY_5, XKB_KEY_percent,                    4),
-	TAGKEYS(          XKB_KEY_6, XKB_KEY_asciicircum,                5),
-	TAGKEYS(          XKB_KEY_7, XKB_KEY_ampersand,                  6),
-	TAGKEYS(          XKB_KEY_8, XKB_KEY_asterisk,                   7),
-	TAGKEYS(          XKB_KEY_9, XKB_KEY_parenleft,                  8),
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Q,          quit,           {0} },
+	/* modifier					 	key				 				function			argument */
+	{ MODKEY,						XKB_KEY_Return,					spawn,				{.v = menucmd} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,	XKB_KEY_Return,					spawn,				{.v = termcmd} },
+	{ MODKEY,						XKB_KEY_b,						togglebar,			{0} },
+	{ MODKEY,						XKB_KEY_Right,					focusstack,			{.i = +1} },
+	{ MODKEY,						XKB_KEY_Left,					focusstack,			{.i = -1} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,	XKB_KEY_Left,					movestack,			{.i = +1} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,	XKB_KEY_Right,					movestack,			{.i = -1} },
+	{ MODKEY,						XKB_KEY_k,						incnmaster,			{.i = +1} },
+	{ MODKEY,						XKB_KEY_l,						incnmaster,			{.i = -1} },
+	{ MODKEY,						XKB_KEY_comma,					setmfact,			{.f = -0.05f} },
+	{ MODKEY,						XKB_KEY_period,					setmfact,			{.f = +0.05f} },
+	{ MODKEY,						XKB_KEY_Return,					zoom,				{0} },
+	{ MODKEY,						XKB_KEY_Tab,					view,				{0} },
+	{ MODKEY,						XKB_KEY_q,						killclient,			{0} },
+	{ MODKEY,						XKB_KEY_t,						setlayout,			{.v = &layouts[0]} },
+	{ MODKEY,						XKB_KEY_c,						togglefloating, 	{0} },
+	{ MODKEY,						XKB_KEY_f,						togglefullscreen,	{0} },
+	//{ MODKEY,						XKB_KEY_x,						movecenter,			{0} },
+	{ MODKEY,						XKB_KEY_0,						view,				{.ui = ~0} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,	XKB_KEY_parenright, 			tag,				{.ui = ~0} },
+	{ MODKEY,						XKB_KEY_comma,					focusmon,			{.i = WLR_DIRECTION_LEFT} },
+	{ MODKEY,						XKB_KEY_period,					focusmon,			{.i = WLR_DIRECTION_RIGHT} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,	XKB_KEY_less,					tagmon,				{.i = WLR_DIRECTION_LEFT} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,	XKB_KEY_greater,				tagmon,				{.i = WLR_DIRECTION_RIGHT} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,	XKB_KEY_Q,						quit,				{0} },
+	{ 0,							XKB_KEY_XF86AudioRaiseVolume,	spawn,				{.v = volupcmd} },
+	{ 0,							XKB_KEY_XF86AudioLowerVolume,	spawn,				{.v = voldowncmd} },
+	{ 0,							XKB_KEY_XF86AudioPlay,			spawn,				{.v = playertogglecmd} },
+	{ 0,							XKB_KEY_XF86AudioNext,			spawn,				{.v = playernextcmd} },
+	{ 0,							XKB_KEY_XF86AudioPrev,			spawn,				{.v = playerprevcmd} },
+	{ MODKEY|WLR_MODIFIER_SHIFT,	XKB_KEY_S,						spawn,				{.v = shotcmd} },
+	TAGKEYS(		  XKB_KEY_1,	XKB_KEY_exclam,						0),
+	TAGKEYS(		  XKB_KEY_2,	XKB_KEY_quotedbl,					1),
+	TAGKEYS(		  XKB_KEY_3,	XKB_KEY_section,					2),
+	TAGKEYS(		  XKB_KEY_4,	XKB_KEY_dollar,						3),
+	TAGKEYS(		  XKB_KEY_5,	XKB_KEY_percent,					4),
+	TAGKEYS(		  XKB_KEY_6,	XKB_KEY_asciicircum,				5),
+	TAGKEYS(		  XKB_KEY_7,	XKB_KEY_slash,						6),
+	TAGKEYS(		  XKB_KEY_8,	XKB_KEY_parenleft,					7),
+	TAGKEYS(		  XKB_KEY_9,	XKB_KEY_parenright,					8),
 
 	/* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
 	{ WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
